62ceb6ab451b9f94d24d2117d6661067946930d1
wd-milmake <112769870+wd-milmake@users.noreply.github.com>
2022-09-04 02:25:45 +0900

    Add files via upload

--- jquery/main.js
+++ jquery/main.js
@@ -6,33 +6,49 @@ $(function () {
             case 1:
                 active_switch();
                 $('#audio_syukka').get(0).play();
+                $('#button').text("どっち!?");
                 break;
             case 2:
                 active_switch();
+                $('#audio_syukka').get(0).pause();
                 $('#audio_docchi').get(0).play();
+                $('#button').text("ふぅぅーっ!");
                 break;
             case 3:
                 active_switch();
+                $('#audio_docchi').get(0).pause();
                 $('#audio_hu').get(0).play();
+                $('#button').text("せぇぇーっ!");
                 break;
             case 4:
                 active_switch();
+                $('#audio_hu').get(0).pause();
                 $('#audio_se').get(0).play();
+                $('#button').text("ぐぅっ！!");
                 break;
             case 5:
                 active_switch();
+                $('#audio_se').get(0).pause();
                 $('#audio_gu').get(0).play();
+                $('#button').text("ぅわたぁ!!");
                 break;
             case 6:
                 active_switch();
+                $('#audio_gu').get(0).pause();
                 $('#audio_wata').get(0).play();
+                $('#button').text("ハッ");
                 break;
             case 7:
                 active_switch();
+                $('#audio_wata').get(0).pause();
                 $('#audio_ha').get(0).play();
+                $('#button').text("もう一回ふせぐ？");
                 break;
             default:
+                $('#audio_ha').get(0).pause();
                 active_reset();
+                voice_reset();
+                $('#button').text("出荷をふせぐ？");
         }
     });
 
@@ -51,5 +67,15 @@ $(function () {
         $('li:nth-child(1)').addClass("active");
         i = 0;
         console.log("reset");
+    }
+
+    function voice_reset() {
+                $('#audio_syukka').get(0).currentTime = 0;
+                $('#audio_docchi').get(0).currentTime = 0;
+                $('#audio_hu').get(0).currentTime = 0;
+                $('#audio_se').get(0).currentTime = 0;
+                $('#audio_gu').get(0).currentTime = 0;
+                $('#audio_wata').get(0).currentTime = 0;
+                $('#audio_ha').get(0).currentTime = 0;
     }
 });
