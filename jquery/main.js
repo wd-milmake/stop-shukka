e646014af845404d62bf262d0430106b11fe9004
wd-milmake <112769870+wd-milmake@users.noreply.github.com>
2022-09-04 02:35:58 +0900

    Add files via upload

--- jquery/main.js
+++ jquery/main.js
@@ -5,37 +5,44 @@ $(function () {
         switch (i) {
             case 1:
                 active_switch();
+                $('#audio_syukka').get(0).currentTime = 0;
                 $('#audio_syukka').get(0).play();
                 $('#button').text("どっち!?");
                 break;
             case 2:
                 active_switch();
                 $('#audio_syukka').get(0).pause();
+                $('#audio_docchi').get(0).currentTime = 0;
                 $('#audio_docchi').get(0).play();
                 $('#button').text("ふぅぅーっ!");
                 break;
             case 3:
                 active_switch();
                 $('#audio_docchi').get(0).pause();
+                $('#audio_hu').get(0).currentTime = 0;
                 $('#audio_hu').get(0).play();
                 $('#button').text("せぇぇーっ!");
                 break;
             case 4:
                 active_switch();
                 $('#audio_hu').get(0).pause();
+                $('#audio_se').get(0).currentTime = 0;
                 $('#audio_se').get(0).play();
                 $('#button').text("ぐぅっ！!");
                 break;
             case 5:
                 active_switch();
                 $('#audio_se').get(0).pause();
+                $('#audio_gu').get(0).currentTime = 0;
                 $('#audio_gu').get(0).play();
                 $('#button').text("ぅわたぁ!!");
                 break;
             case 6:
                 active_switch();
                 $('#audio_gu').get(0).pause();
+                $('#audio_wata').get(0).currentTime = 0;
                 $('#audio_wata').get(0).play();
+                $('#audio_ha').get(0).currentTime = 0;
                 $('#button').text("ハッ");
                 break;
             case 7:
@@ -47,7 +54,7 @@ $(function () {
             default:
                 $('#audio_ha').get(0).pause();
                 active_reset();
-                voice_reset();
+                // voice_reset();
                 $('#button').text("出荷をふせぐ？");
         }
     });
